d395dda7c848963b9fe9fb851dfe732cbeeddced
leon <leon00albert@gmail.com>
2024-02-23 20:43:06 +0100

    add ts exercise

--- ts/01-manager.js
+++ ts/01-manager.js
@@ -13,40 +13,24 @@ Implement a class named 'ProductManager' that:
     has a method 'getProductById' that takes an ID (number) as a parameter and returns the corresponding 'Product' object if found, otherwise returns null.
 
 */
-
-interface Product  {
-    id : number
-    name: string,
-    price: number
-    description: string
-}
-
-class ProductManager {
-    private products: Product[];
-
-    constructor(){
+var ProductManager = /** @class */ (function () {
+    function ProductManager() {
         this.products = [];
     }
-
-    addProduct(product :Product ):void
-    {
-        this.products.push(product)
-    }
-    getProductById(id:number):Product | undefined
-    {
-        return this.products.find( p => p.id === id);
-    }
-
-}
-
-
-const manager = new ProductManager();
-const product:Product = {
+    ProductManager.prototype.addProduct = function (product) {
+        this.products.push(product);
+    };
+    ProductManager.prototype.getProductById = function (id) {
+        return this.products.find(function (p) { return p.id === id; });
+    };
+    return ProductManager;
+}());
+var manager = new ProductManager();
+var product = {
     id: 0,
     name: 'test',
     price: 10,
     description: 'test'
-}
-
+};
 manager.addProduct(product);
 console.log(manager.getProductById(1));
